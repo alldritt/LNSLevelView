435c33008acb0d6a9839a6d3906a1a745306d6d4
Mark Alldritt <alldritt@latenightsw.com>
2022-02-04 15:18:31 -0800

    Get SPM package compiling and refine code.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LNSLevelView",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v7)
+        .macOS(.v10_15), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -23,6 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LNSLevelView",
-            dependencies: []),
+            dependencies: [])
     ]
 )
--- Sources/LNSLevelView/LNSLevelView.swift
+++ Sources/LNSLevelView/LNSLevelView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 
 fileprivate extension CGColor {
-    /// The RGBA components associated with a `UIColor` instance.
+
     var rgbComponents: (r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
         let components = self.components!
 
@@ -19,13 +19,6 @@ fileprivate extension CGColor {
         }
     }
 
-    /**
-     Returns a `UIColor` by interpolating between two other `UIColor`s.
-     - Parameter fromColor: The `UIColor` to interpolate from
-     - Parameter toColor:   The `UIColor` to interpolate to (e.g. when fully interpolated)
-     - Parameter progress:  The interpolation progess; must be a `CGFloat` from 0 to 1
-     - Returns: The interpolated `UIColor` for the given progress point
-     */
     static func interpolate(from fromColor: CGColor, to toColor: CGColor, with progress: CGFloat) -> CGColor {
         let fromComponents = fromColor.rgbComponents
         let toComponents = toColor.rgbComponents
@@ -41,62 +34,77 @@ fileprivate extension CGColor {
 
 
 public struct LNSLevelView: View {
-    public @Binding var value: CGFloat
+    @Binding public var value: CGFloat
     public let maxValue: CGFloat
-    public var backgroundColor = Gradient(colors: [Color.green.opacity(0.2)]) // default background gradient
-    public var foregroundColor = Gradient(colors: [Color.green]) // default value bar gradient
-    public var animationDuration = TimeInterval(1) // default animation: 1s
+    public let backgroundColor: Gradient // background gradient
+    public var foregroundColor: Gradient // value bar gradient
+    public var animationDuration: TimeInterval
+    
+    public init(value: Binding<CGFloat>,
+                maxValue: CGFloat,
+                backgroundColor: Gradient = .init(colors: [Color.green.opacity(0.2)]),
+                foregroundColor: Gradient = .init(colors: [Color.green]),
+                animationDuration: TimeInterval = 1) {
+        self._value = value
+        self.maxValue = maxValue
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+        self.animationDuration = animationDuration
+    }
 
-    private func level(value: CGFloat, height: CGFloat) -> CGFloat {
-        let percentage = max(0, min(value, maxValue)) / maxValue
+    @inline(__always)
+    private var percentage: CGFloat {
+        return max(0, min(value, maxValue)) / maxValue
+    }
+    
+    @inline(__always)
+    private func level(height: CGFloat) -> CGFloat {
         return height * percentage
     }
     
     private var barColor: Color {
         //  Find the two segments and calculate the interpolated color...  The assumption
         //  is that location increases with each stop.
-        var baseValue = foregroundColor.stops[0].location
-        
+        let percentage = self.percentage
+
         for i in 1..<foregroundColor.stops.count {
-            if value >= foregroundColor.stops[i - 1].location &&
-                value <= foregroundColor.stops[i].location {
+            if percentage >= foregroundColor.stops[i - 1].location &&
+                percentage <= foregroundColor.stops[i].location {
                 let fromColor = foregroundColor.stops[i - 1].color.cgColor
                 let toColor = foregroundColor.stops[i].color.cgColor
-                
+                let baseValue = foregroundColor.stops[i - 1].location
+
                 let delta = foregroundColor.stops[i].location - foregroundColor.stops[i - 1].location
-                let v = (value - baseValue) / delta
+                let v = (percentage - baseValue) / delta
                 
                 return Color(CGColor.interpolate(from: fromColor!, to: toColor!, with: v))
             }
-            
-            baseValue = foregroundColor.stops[i].location
         }
         
         return foregroundColor.stops[0].color
     }
     
     public var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            GeometryReader { geometryReader in
-                let level = level(value: value,
-                                  height: geometryReader.size.height)
-                let backgroundColor = LinearGradient(gradient: backgroundColor, startPoint: .bottom, endPoint: .top)
-                let barColor = barColor
+        GeometryReader { geometryReader in
+            let level = level(height: geometryReader.size.height)
+            let backgroundColor = LinearGradient(gradient: backgroundColor, startPoint: .bottom, endPoint: .top)
+            let barColor = barColor
 
-                Rectangle()
-                    .fill(backgroundColor)
-                    .overlay(
-                        Rectangle()
-                            .fill(barColor)
-                            .frame(height: level),
-                        alignment: .bottom
-                    )
-                    .overlay(
-                        Rectangle()
-                            .stroke(barColor, lineWidth: 0.5)
-                    )
-                    .animation(Animation.easeInOut(duration: animationDuration), value: value)
-            }
+            Rectangle()
+                .fill(backgroundColor)
+                .overlay(
+                    Rectangle()
+                        .fill(barColor)
+                        .frame(height: level),
+                    alignment: .bottom
+                )
+                .overlay(
+                    Rectangle()
+                        .stroke(barColor, lineWidth: 0.5)
+                )
+                .animation(Animation.easeInOut(duration: animationDuration), value: value)
         }
     }
 }
+
+
