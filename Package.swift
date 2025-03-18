b04091afb4b079a6cc8be657d244c2abb5066d87
Mark Alldritt <alldritt@latenightsw.com>
2025-03-17 17:38:45 -0700

    Standardize min OS versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LNSLevelView",
     platforms: [
-        .macOS(.v10_15), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
+        .macOS(.v11), .iOS(.v14), .tvOS(.v13), .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
